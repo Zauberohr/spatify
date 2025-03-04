7f6f4cd89d4a34ad04bece991509fb12af70a20e
ahlam barhoun <ahlambj2000@gmail.com>
2025-03-04 17:14:56 +0100

    code for the user to create an account

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :password, presence: true, length: { minimum: 6 }
 end
