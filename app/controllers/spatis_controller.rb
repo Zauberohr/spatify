b5f0361d086af6ee4f785bb5020dac7dbd9081e1
Naakul Vashist <nakul1851@gmail.com>
2025-03-05 16:33:41 +0100

    added map on index and show page

--- app/controllers/spatis_controller.rb
+++ app/controllers/spatis_controller.rb
@@ -2,9 +2,20 @@ class SpatisController < ApplicationController
 
   def index
     @spatis = Spati.all
+    @markers = @spatis.geocoded.map do |spati|
+      {
+        lat: spati.latitude,
+        lng: spati.longitude,
+        info_window_html: render_to_string(partial: "info_window", locals: {spati: spati})
+      }
+    end
   end
-    
+
   def show
     @spati = Spati.find(params[:id])
+    @markers = [{
+      lat: @spati.latitude,
+      lng: @spati.longitude
+    }]
   end
 end
